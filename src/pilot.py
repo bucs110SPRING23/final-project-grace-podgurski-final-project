c9e929dd57bb07ef87dad8cfe9c2827a04b2acc2
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-26 11:11:38 -0400

    still working but want to save

---
+++ src/pilot.py
@@ -0,0 +1,27 @@
+
+
+class Pilot:
+  starting_score = 0
+  starting_point = int[0,0]
+
+  def __init__(self,x,y,shape,size,color):
+    self.x = x
+    self.y = y
+    self.shape = shape
+    self.size = size
+    self.direction = "right"
+    self.color = color
+
+  def move_right(self):
+    if self.direction == "right":
+        self.x -= 1
+    else:
+        self.x += 1
+
+  def move_up(self):
+     pass
+  
+  def move_down(self):
+     pass
+  
+  
--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,16 +1,23 @@
 import pygame
 import pygame_menu
 
-
+from src.pilot import Pilot
 
 class Controller:
   
   def __init__(self):
     #setup pygame data
-    
+    pygame.init()
+
+    self.screen = pygame.display.set_mode()
+    size = pygame.display.get_window_size()
+
+    self.width = size[0]
+    self.height = size[1]
+
   def mainloop(self):
     #select state loop
-    pygame.init
+
   
   ### below are some sample loop states ###
 
